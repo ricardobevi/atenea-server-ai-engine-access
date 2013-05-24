81c35c7458f70c427de2c9c80f1c4f9ac054c42e
ric <ric@localhost>
2013-05-23 23:19:09 -0300

    Cambio para que inicie la bd apenas se instancia el objeto. POM para compilar.

--- src/main/java/org/atenea/aiengineaccess/AIEngineAccessFacade.java
+++ src/main/java/org/atenea/aiengineaccess/AIEngineAccessFacade.java
@@ -1,8 +1,5 @@
 package org.atenea.aiengineaccess;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.atenea.base.Log;
 import org.scuadra.atenea.aiengine.AIEngineFacade;
 import org.scuadra.atenea.aiengine.Message;
 
@@ -10,13 +7,18 @@ import org.scuadra.atenea.aiengine.Message;
 
 public class AIEngineAccessFacade {
 	
-	private Logger logger = LogManager.getLogger(this.getClass());
+	//private Logger logger = LogManager.getLogger(this.getClass());
+	
+	private AIEngineFacade aiEngineFacade;
+	
+	public AIEngineAccessFacade(){
+		AIEngineFacade aiEngineFacade = new AIEngineFacade();
+	}
+	
 	
 	public String excecute(String input) {
 		
 		Message message = new Message(input);
-		
-		AIEngineFacade aiEngineFacade = new AIEngineFacade();
 		
 		return aiEngineFacade.execute(message).getText();
 
